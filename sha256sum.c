c9633bd95fe7d1f9d42dd53f77309b4cf05b90ef
simransinghal <singhalsimran0@gmail.com>
2018-04-03 21:26:47 +0530

    update code

--- sha256sum.c
+++ sha256sum.c
@@ -16,8 +16,8 @@ int main(int argc, const char **argv)
 {
 	struct ceph_mount_info *cmount;
 	int ret, fd, len;
-	char buffer[BUFSIZ];
-	unsigned char check[SHA256_DIGEST_LENGTH];
+	char buffer[102];
+	unsigned char hash[SHA256_DIGEST_LENGTH];
 
 	if (argc < 3) {
 		fprintf(stderr, "usage: %s <conf> <file>\n", argv[0]);
@@ -48,38 +48,66 @@ int main(int argc, const char **argv)
 		exit(1);
 	}
 
-        ret = ceph_chdir(cmount, "/");
-        if (ret) {
-                fprintf(stderr, "ceph_chdir=%d\n", ret);
-                exit(1);
-        }
+	ret = ceph_chdir(cmount, "/");
+	if (ret) {
+		fprintf(stderr, "ceph_chdir=%d\n", ret);
+		exit(1);
+	}
 
+
+	fd = ceph_open(cmount, argv[2], O_CREAT|O_TRUNC|O_RDWR, 0777); 
+	if (fd < 0) {
+		fprintf(stderr, "ceph_open=%d\n", fd);
+		exit(1);
+	}
+
+        memset(buffer, 'b', sizeof(buffer));
+
+	len = ceph_write(cmount, fd, buffer, sizeof(buffer), 0);
+	if (len < 0) {
+		fprintf(stderr, "ceph_write=%d\n", len);
+		exit(1);
+	}
+
+	fprintf(stdout, "wrote %d bytes\n", len);
 	
-        fd = ceph_open(cmount, argv[2], O_CREAT|O_TRUNC|O_RDWR, 0777); 
+	ret = ceph_close(cmount, fd);
+	if(ret){
+                fprintf(stderr, "ceph_close=%d\n", ret);
+                exit(1);
+	}
+
+        fd = ceph_open(cmount, argv[2], O_RDONLY, 0777); 
         if (fd < 0) {
                 fprintf(stderr, "ceph_open=%d\n", fd);
                 exit(1);
+	}
+
+	len = ceph_read(cmount, fd, buffer, sizeof(buffer), 0);
+	if (len < 0) {
+                fprintf(stderr, "ceph_read=%d\n", len);
+                exit(1);
+	}
+
+	fprintf(stdout, "read %d bytes\n", len);
+
+        ret = ceph_close(cmount, fd);
+        if(ret){
+                fprintf(stderr, "ceph_close=%d\n", ret);
+                exit(1);
         }
 
-	SHA256_CTX ctx;
-	SHA256_Init(&ctx);
+	ceph_shutdown(cmount);
 
-	do {
-		len = ceph_read (cmount, fd, buffer, BUFSIZ, -2);	
-		if(len < 0)
-			break;						
-		SHA256_Update(&ctx, buffer, len);
-	} while (len == BUFSIZ);			
+        SHA256_CTX sha256;
+        SHA256_Init(&sha256);
+	SHA256_Update(&sha256, buffer, len);
+	SHA256_Final(hash, &sha256);
 	
-	SHA256_Final(check, &ctx);
-	
-	ceph_shutdown(cmount);	
-
 	for (len = 0; len < SHA256_DIGEST_LENGTH; ++len)
-		printf("%02x", check[len]);
+		printf("%02x", hash[len]);
 
 	putchar('\n');	
 
 	return 0;
 }
-
